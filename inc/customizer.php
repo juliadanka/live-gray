91f81e97a6473864991c1f8bbe60d9ade730c117
Iulia Danilevska <juliadanka1@gmail.com>
2018-02-26 17:35:05 +0200

    customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -10,22 +10,7 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function live_gray_customize_register($wp_customize)
-{
-//    $wp_customize->get_setting('blogname')->transport = 'postMessage';
-//    $wp_customize->get_setting('blogdescription')->transport = 'postMessage';
-////    $wp_customize->get_setting('header_textcolor')->transport = 'postMessage';
-//
-//    if (isset($wp_customize->selective_refresh)) {
-//        $wp_customize->selective_refresh->add_partial('blogname', array(
-//            'selector' => '.site-title a',
-//            'render_callback' => 'live_gray_customize_partial_blogname',
-//        ));
-//        $wp_customize->selective_refresh->add_partial('blogdescription', array(
-//            'selector' => '.site-description',
-//            'render_callback' => 'live_gray_customize_partial_blogdescription',
-//        ));
-//    }
+function live_gray_customize_register($wp_customize){
     // Add main Site settings and control.
     $wp_customize->add_section('live_gray_sosial', array(
         'title' => esc_html__('Social links', 'live-gray'),
@@ -80,33 +65,3 @@ function live_gray_customize_register($wp_customize)
 }
 
 add_action('customize_register', 'live_gray_customize_register');
-
-/**
-// * Render the site title for the selective refresh partial.
-// *
-// * @return void
-// */
-//function live_gray_customize_partial_blogname()
-//{
-//    bloginfo('name');
-//}
-//
-///**
-// * Render the site tagline for the selective refresh partial.
-// *
-// * @return void
-// */
-//function live_gray_customize_partial_blogdescription()
-//{
-//    bloginfo('description');
-//}
-//
-///**
-// * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
-// */
-//function live_gray_customize_preview_js()
-//{
-//    wp_enqueue_script('live-gray-customizer', get_template_directory_uri() . '/js/customizer.js', array('customize-preview'), '20151215', true);
-//}
-//
-//add_action('customize_preview_init', 'live_gray_customize_preview_js');
